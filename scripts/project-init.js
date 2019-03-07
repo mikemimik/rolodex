d6f27da1ff620e434c611aca2ba255fc98238a1d
Michael Perrotte <michael.perrotte@resolver.com>
2019-03-07 02:04:38 -0500

    feat(project,scripts): added init scripts

---
+++ scripts/project-init.js
@@ -0,0 +1,36 @@
+/* global prompt */
+
+exports.name = prompt('Project Name', 'ulysses-app', (name) => name.split(' ').join('-').toLowerCase());
+
+exports.repository = prompt(
+  'GitHub [username/repository]',
+  'HackerYou/roject-repo',
+  (data) => {
+    const [username, repository] = data.split('/');
+
+    // Mutate the global `this` object
+    Object.assign(this, { context: { username, repository } });
+
+    const repoObject = {
+      type: 'git',
+      url: `git+https://github.com/${username}/${repository}.git`,
+    };
+    return repoObject;
+  }
+);
+
+exports.author = prompt('Author', '', (author) => author);
+
+exports.scripts = (cb) => {
+  const { package: { scripts: currentScripts }, context } = this;
+  const { username, repository } = context;
+  const scripts = {
+    'git:remote:set': `git remote set-url https://github.com/${username}/${repository}.git`,
+  };
+
+  // Remove inital scripts
+  delete currentScripts.init;
+  delete currentScripts.postinit;
+
+  cb(null, Object.assign({}, currentScripts, scripts));
+};
