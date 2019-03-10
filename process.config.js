6ad7f0999c3f210a9fb9b7712a41fa810e6e9dd2
Michael Perrotte <michael.perrotte@resolver.com>
2019-03-09 20:28:05 -0500

    feat(project): added pm2 process file

---
+++ process.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'api',
+      script: './api/server.js',
+      watch: true,
+      env: {
+        'NODE_ENV': 'development',
+        'PORT': 4000,
+      },
+      kill_timeout: 3000,
+      wait_ready: true,
+    },
+  ],
+};
