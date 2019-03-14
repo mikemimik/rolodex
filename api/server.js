8f15db6d4467fb6ed3033009e490851249748b4f
Michael Perrotte <michael.perrotte@resolver.com>
2019-03-13 23:53:17 -0400

    docs(project): added comments to server.js

--- api/server.js
+++ api/server.js
@@ -3,21 +3,36 @@
 const express = require('express');
 const http = require('http');
 
+// 1. Create main express intance
 const router = express();
 
-const { applyMiddleware, appleRoutes } = require('./utils');
-const middleWare = require('./middleware');
-const errorHandlers = require('./middleware/errorHandlers');
-const bookRoutes = require('./routes/books/bookRoutes');
+// 2. Require utility function for adding middleware
+const { applyMiddleware } = require('./utils');
 
+// 3a. Require general middleware
+const middleWare = require('./middleware');
+// 3b. Require error handling middleware
+const errorHandlers = require('./middleware/errorHandlers');
+
+// 4. Require routes
+const { router: bookRoutes } = require('./routes/books/bookRoutes');
+
+// 5. Require conatants
 const { PORT } = require('./utils/constants');
 
+// 6. Apply general middleware
 applyMiddleware(middleWare, router);
-appleRoutes(routes);
+
+// 7. Utilise routes
+router.use('/books', bookRoutes);
+
+// 8. Apply error handling middleware (meaningfully last)
 applyMiddleware(errorHandlers, router);
 
+// 9. Create a server from express instance
 const server = http.createServer(router);
 
+// 10. Start server
 server.listen(PORT, () => {
   console.log(`Server is running on PORT:${PORT}`);
   process.send('ready');
