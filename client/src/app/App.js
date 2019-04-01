bcfac4d31e7bb6ea7e6390a332d1de7084cedade
Michael Perrotte <michael.perrotte@resolver.com>
2019-04-01 01:22:20 -0400

    feat: moved App component into separate folder

---
+++ client/src/app/App.js
@@ -0,0 +1,35 @@
+import React, { Component, Fragment } from 'react';
+import logo from '../assets/logo.svg';
+import './App.css';
+
+import { withStyles } from '@material-ui/core/styles';
+
+import Header from '../header/Header';
+
+class App extends Component {
+  render () {
+    return (
+      <Fragment>
+        <Header />
+        <div className='App'>
+          <header className='App-header'>
+            <img src={logo} className='App-logo' alt='logo' />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className='App-link'
+              href='https://reactjs.org'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
