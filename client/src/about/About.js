decacff3e316a96fdd1e5f15c1234e5cf4b2fed4
Michael Perrotte <michael.perrotte@resolver.com>
2019-04-01 01:38:56 -0400

    feat: added About component; initial

---
+++ client/src/about/About.js
@@ -0,0 +1,13 @@
+import React, { PureComponent, Fragment } from 'react';
+
+class About extends PureComponent {
+  render () {
+    return (
+      <Fragment>
+        <h1>About</h1>
+      </Fragment>
+    );
+  }
+}
+
+export default About;
