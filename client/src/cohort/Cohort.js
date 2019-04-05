3366c09eb0c3aaf9c7da995f1a2f883760067b56
Michael Perrotte <michael.perrotte@resolver.com>
2019-04-05 01:17:13 -0400

    feat(client,Cohort): added Cohort component

---
+++ client/src/cohort/Cohort.js
@@ -0,0 +1,19 @@
+import React, { PureComponent } from 'react';
+
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = (theme) => ({
+
+});
+
+class Cohort extends PureComponent {
+  render() {
+    return (
+      <React.Fragment>
+        Cohort
+      </React.Fragment>
+    );
+  }
+}
+
+export default withStyles(styles)(Cohort);
