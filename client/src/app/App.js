d76def2602de13851576d602a86e7cd82892908a
Michael Perrotte <michael.perrotte@resolver.com>
2019-04-05 00:39:52 -0400

    feat(client,App): updated App component; removed unused code, added Cohorts component

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,32 +1,16 @@
 import React, { Component, Fragment } from 'react';
-import logo from '../assets/logo.svg';
-import './App.css';
 
 import { withStyles } from '@material-ui/core/styles';
 
 import Header from '../header/Header';
+import Cohorts from '../cohort/Cohorts';
 
 class App extends Component {
   render () {
     return (
       <Fragment>
         <Header />
-        <div className='App'>
-          <header className='App-header'>
-            <img src={logo} className='App-logo' alt='logo' />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className='App-link'
-              href='https://reactjs.org'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
+        <Cohorts />
       </Fragment>
     );
   }
