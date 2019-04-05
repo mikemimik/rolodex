40444c1aeef71bbb81b985bd097a99ad51e998fa
Michael Perrotte <michael.perrotte@resolver.com>
2019-04-05 01:16:52 -0400

    feat(client,App): added state; added onFetchCohorts, handleTabChange methods; passed down props

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,4 +1,7 @@
 import React, { Component, Fragment } from 'react';
+import {
+  uniqBy,
+} from 'lodash';
 
 import { withStyles } from '@material-ui/core/styles';
 
@@ -6,11 +9,50 @@ import Header from '../header/Header';
 import Cohorts from '../cohort/Cohorts';
 
 class App extends Component {
+  state = {
+    currentView: 'view',
+    cohorts: [],
+    cohortsById: {},
+  };
+
+  onFetchCohorts = (cohorts) => {
+    const cohortsById = cohorts.reduce((acc, cohort) => {
+      const { _id } = cohort;
+      return Object.assign({}, acc, { [_id]: cohort });
+    }, {});
+
+    const currentCohorts = this.state.cohorts;
+    const nextCohorts = uniqBy([...currentCohorts, ...cohorts], '_id');
+
+    const currentCohortsById = this.state.cohortsById;
+    const nextCohortsById = Object.assign({}, currentCohortsById, cohortsById);
+
+    this.setState({
+      cohorts: nextCohorts,
+      cohortsById: nextCohortsById,
+    });
+  }
+
+  handleTabChange = (event, value) => {
+    this.setState({
+      currentView: value,
+    });
+  }
+
   render () {
     return (
       <Fragment>
-        <Header />
-        <Cohorts />
+        <Header
+          currentView={this.state.currentView}
+          handleTabChange={this.handleTabChange}
+        />
+        {
+          this.state.currentView === 'view' &&
+          <Cohorts
+            cohorts={this.state.cohorts}
+            onFetchCohorts={this.onFetchCohorts}
+          />
+        }
       </Fragment>
     );
   }
