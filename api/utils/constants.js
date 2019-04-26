b61a8f70afca71d186a2a6c64e30569d14e07faf
Michael Perrotte <michael.perrotte@resolver.com>
2019-04-26 03:15:22 -0400

    feat(api,constants): updated mongodb constant

--- api/utils/constants.js
+++ api/utils/constants.js
@@ -1,6 +1,6 @@
 
 'use strict';
 
-exports.URL = process.env.MONGO_DB_URL || 'mongodb://localhost:27017/rolodex';
+exports.URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/rolodex';
 exports.PORT = process.env.PORT || 3001;
 exports.SECRET = process.env.SECRET || 'super-secret-passphrase';
