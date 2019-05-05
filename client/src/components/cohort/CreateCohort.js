65a4859c724c5a9af4fa6ecac7c26c5d2ffa6533
Michael Perrotte <michael.perrotte@resolver.com>
2019-05-05 01:11:01 -0400

    feat(client,createcohorts): updated CreateCohort component to create new cohorts

--- client/src/components/cohort/CreateCohort.js
+++ client/src/components/cohort/CreateCohort.js
@@ -1,15 +1,76 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 
+import {
+  Button,
+  MenuItem,
+  TextField,
+} from '@material-ui/core';
+
 import { listTabs, handleTabChange } from '../Tabs';
+import { getToken } from '../../services/tokenService';
 import Header from '../Header';
 import Page from '../Page';
 
 const styles = (theme) => ({
   root: {},
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+  },
+  menu: {
+    width: 200,
+  },
 });
+export const CohortTypes = [
+  'spring',
+  'summer',
+  'fall',
+  'winter',
+];
 
 class CreateCohort extends PureComponent {
+  state = {
+    nextCohort: {
+      cohort: null,
+      program: '',
+      year: null,
+    },
+  };
+
+  submit = async () => {
+    try {
+      const { history } = this.props;
+      const { nextCohort } = this.state;
+      const token = getToken();
+      await fetch('/api/cohorts', {
+        method: 'POST',
+        body: JSON.stringify(nextCohort),
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      });
+      return history.push({ pathname: '/cohorts' });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  handleInputChange = (prop) => ({ target: { id, value } }) => {
+    console.groupEnd();
+    const currentCohort = this.state.nextCohort;
+    const nextCohort = { [prop]: value };
+    this.setState({
+      nextCohort: Object.assign({}, currentCohort, nextCohort),
+    });
+  };
+
   renderHeader = (props) => (
     <Header
       currentView={'cohorts.create'}
@@ -23,15 +84,70 @@ class CreateCohort extends PureComponent {
   )
 
   render () {
+    const { classes } = this.props;
     return (
       <>
         {this.renderHeader(this.props)}
         <Page title='Create Cohort'>
-          <div />
+          <form className={classes.container}>
+            <TextField
+              fullWidth
+              id='program'
+              label='Program Name'
+              className={classes.textField}
+              value={this.state.nextCohort.program}
+              onChange={this.handleInputChange('program')}
+              helperText='Please enter program name'
+            />
+            <TextField
+              fullWidth
+              id='year'
+              label='Cohort Year'
+              className={classes.textField}
+              value={this.state.nextCohort.year}
+              onChange={this.handleInputChange('year')}
+              helperText='Please enter cohort year'
+            />
+            <TextField
+              select
+              fullWidth
+              id='cohort'
+              label='Cohort Type'
+              className={classes.textField}
+              value={this.state.nextCohort.cohort || CohortTypes[0]}
+              onChange={this.handleInputChange('cohort')}
+              SelectProps={{
+                MenuProps: {
+                  className: classes.menu,
+                },
+              }}
+              helperText='Please select a cohort'
+              margin='normal'
+            >
+              {CohortTypes.map((option) => (
+                <MenuItem key={option} value={option}>
+                  {option}
+                </MenuItem>
+              ))}
+            </TextField>
+            <Button
+              fullWidth
+              variant='contained'
+              color='primary'
+              className={classes.submit}
+              onClick={this.submit}
+            >
+              Create Cohort
+            </Button>
+          </form>
         </Page>
       </>
     );
   }
 }
 
+CreateCohort.propTypes = {
+  classes: PropTypes.object,
+  history: PropTypes.object,
+};
 export default withStyles(styles)(CreateCohort);
