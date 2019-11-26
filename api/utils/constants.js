df6a38d97d0912b545e51fd44dd0b4f3c56de094
Michael Perrotte <mike@npmjs.com>
2019-11-26 01:41:35 -0500

    fix(api,constants): changed default port

--- api/utils/constants.js
+++ api/utils/constants.js
@@ -2,6 +2,6 @@
 'use strict';
 
 exports.URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/rolodex';
-exports.PORT = process.env.PORT || 3001;
+exports.PORT = process.env.PORT || 4000;
 exports.SECRET = process.env.SECRET || 'super-secret-passphrase';
 exports.PUBLIC_FOLDER = (process.env.NODE_ENV === 'production') ? 'build' : 'public';
