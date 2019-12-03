a2ea262bb4147fd742913ae66691611c68df28e7
Michael Perrotte <mike@npmjs.com>
2019-12-02 21:23:59 -0500

    feat: added build folder, so heroku stops blowing up

---
+++ build/precache-manifest.8a15339b28f6f2e79807240c7a8f3dea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b123ce35020bf4e7d4f315d812fc380",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03a730d35761ae16ed11",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "44558af4d0667d3dbbab",
+    "url": "/static/js/2.8b1bf04d.chunk.js"
+  },
+  {
+    "revision": "03a730d35761ae16ed11",
+    "url": "/static/js/main.bc23e0bd.chunk.js"
+  },
+  {
+    "revision": "eda9107b7d04145aa222",
+    "url": "/static/js/runtime-main.e867aa22.js"
+  }
+]);
