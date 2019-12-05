abd6ee0b85154bcef6302aa4ef1562d16146496c
Michael Perrotte <mike@npmjs.com>
2019-12-05 00:23:19 -0500

    chore(api,middleware): udpated variable names in error handlers

--- api/middleware/errorHandlers.js
+++ api/middleware/errorHandlers.js
@@ -5,24 +5,24 @@ const path = require('path');
 const { publicPath } = require('./public');
 const ErrorHandler = require('../utils/errorHandler');
 
-const redirect = exports.redirect = (router) => {
-  router.use('/*', (req, res, next) => {
+const redirect = exports.redirect = (app) => {
+  app.use('/*', (req, res, next) => {
     res.sendFile(path.join(publicPath, 'index.html'));
   });
 };
 
-const handle404Error = exports.handle404Error = (router) => {
-  router.use((req, res, next) => {
+const handleAPI404Error = exports.handle404Error = (app) => {
+  app.use('/api/*', (req, res, next) => {
     ErrorHandler.notFoundError();
   });
 };
 
-const handleClientError = exports.handleClientError = (router) => {
-  router.use(ErrorHandler.clientError);
+const handleClientError = exports.handleClientError = (app) => {
+  app.use(ErrorHandler.clientError);
 };
 
-const handleServerError = exports.handleServerError = (router) => {
-  router.use(ErrorHandler.serverError);
+const handleServerError = exports.handleServerError = (app) => {
+  app.use(ErrorHandler.serverError);
 };
 
-module.exports = [redirect, handle404Error, handleClientError, handleServerError];
+module.exports = [handleAPI404Error, redirect, handleClientError, handleServerError];
