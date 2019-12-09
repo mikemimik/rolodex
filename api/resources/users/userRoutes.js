b5b757d3db20f1176fa55aeca0dc417859805a97
Michael Perrotte <mike@npmjs.com>
2019-12-09 04:28:01 -0500

    style(api,route,user): linted

--- api/resources/users/userRoutes.js
+++ api/resources/users/userRoutes.js
@@ -1,65 +1,65 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const userService = require('./userService');
-const tokenService = require('../../utils/tokenService');
-const { logRequest } = require('../../utils');
+const userService = require('./userService')
+const tokenService = require('../../utils/tokenService')
+const { logRequest } = require('../../utils')
 
-const requiresAuth = require('../../middleware/auth');
-const { HTTP401Error, HTTP400Error } = require('../../utils/httpErrors');
+const requiresAuth = require('../../middleware/auth')
+const { HTTP401Error, HTTP400Error } = require('../../utils/httpErrors')
 
 router.route('/')
   .post(async (req, res, next) => {
     try {
-      const user = await userService.createUser(req.body);
+      const user = await userService.createUser(req.body)
       res.status(201).json({
-        data: [user],
-      });
-      logRequest(req, res);
+        data: [user]
+      })
+      logRequest(req, res)
     } catch (e) {
-      next(e);
+      next(e)
     }
-  });
+  })
 
 router.route('/login')
   .post(async (req, res, next) => {
     try {
-      const user = await userService.isUser(req.body);
+      const user = await userService.isUser(req.body)
       if (user) {
-        const token = await tokenService.issueToken(user);
+        const token = await tokenService.issueToken(user)
         res.status(200).json({
           data: [{
-            token,
-          }],
-        });
-        logRequest(req, res);
+            token
+          }]
+        })
+        logRequest(req, res)
       } else {
-        next(new HTTP400Error());
+        next(new HTTP400Error())
       }
     } catch (e) {
-      next(e);
+      next(e)
     }
-  });
+  })
 
 router.route('/me')
   .get(
     requiresAuth,
     async (req, res, next) => {
       try {
-        const { user: { id: userId } } = req.token;
-        const user = await userService.findUser(userId);
+        const { user: { id: userId } } = req.token
+        const user = await userService.findUser(userId)
         if (!user) {
-          next(new HTTP401Error());
+          next(new HTTP401Error())
         } else {
-          res.status(200).json({ data: [user] });
-          logRequest(req, res);
+          res.status(200).json({ data: [user] })
+          logRequest(req, res)
         }
       } catch (e) {
-        next(e);
+        next(e)
       }
     }
-  );
+  )
 
-exports.router = router;
+exports.router = router
