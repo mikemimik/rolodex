30b2101a36655464935d59a7617bdf421feac61c
Michael Perrotte <mike@npmjs.com>
2019-12-09 04:22:35 -0500

    feat(api,seed): updated the seeding file

--- api/utils/seed.js
+++ api/utils/seed.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const pMap = require('p-map')
+
 const { model: Cohort } = require('../resources/cohorts/cohortModel')
 const { model: Student } = require('../resources/students/studentModel')
 const { model: Project } = require('../resources/projects/projectModel')
@@ -10,90 +12,95 @@ exports.truncate = async () => {
   await Student.deleteMany()
   await Project.deleteMany()
   await User.deleteMany()
+  console.log('Truncation complete.')
 }
 
 exports.seed = async () => {
   try {
-    const projectData = [
+    const cohortData = [
       {
-        title: 'rolodex',
-        description: 'A tool used to track students I teach and the projects that they create.',
-        url: ''
-      }
+        year: 2019,
+        cohort: 'winter',
+        program: 'full-stack-master-class',
+        students: [],
+      },
+      {
+        year: 2018,
+        cohort: 'fall',
+        program: 'full-stack-master-class',
+        students: [],
+      },
     ]
-    const projectPromises = projectData.map(async (data) => {
-      try {
-        const project = new Project(data)
-        return project.save()
-      } catch (e) {
-        throw e
-      }
+    console.log('Seeding Cohort Data...')
+    const cohorts = await pMap(cohortData, async (data) => {
+      const cohort = new Cohort(data)
+      await cohort.validate()
+      return cohort.save()
     })
-    const projects = await Promise.all(projectPromises)
+    const cohort = cohorts[0]
 
     const studentData = [
       {
         firstName: 'Michael',
         lastName: 'Perrotte',
         avatar: '',
-        projects
+        projects: [],
+        cohorts: [cohort._id],
       },
       {
         firstName: 'Jamie',
         lastName: 'Scott',
         avatar: '',
-        projects: []
-      }
-    ]
-    const studentPromises = studentData.map(async (data) => {
-      try {
-        const student = new Student(data)
-        return student.save()
-      } catch (e) {
-        throw e
-      }
-    })
-    const students = await Promise.all(studentPromises)
-
-    const cohortData = [
-      {
-        year: 2019,
-        cohort: 'winter',
-        program: 'full-stack-master-class',
-        students
+        projects: [],
+        cohorts: [cohort._id],
       },
-      {
-        year: 2018,
-        cohort: 'fall',
-        program: 'full-stack-master-class',
-        students: []
-      }
     ]
-    const cohortPromises = cohortData.map(async (data) => {
-      try {
-        const cohort = new Cohort(data)
-        return cohort.save()
-      } catch (e) {
-        throw e
-      }
+    console.log('Seeding Student Data...')
+    const students = await pMap(studentData, async (data) => {
+      const student = new Student(data)
+      await student.validate()
+      return student.save()
     })
-    await Promise.all(cohortPromises)
+    const student = students[0]
+
+    console.log('Updating Cohort Data (w/ Student Data)...')
+    cohort.students = students.map((s) => s._id)
+    await cohort.validate()
+    await cohort.save()
+
+    const projectData = [
+      {
+        title: 'rolodex',
+        description: 'A tool used to track students I teach and the projects that they create.',
+        url: 'https://mikemimik-rolodex.herokuapp.com',
+        student: student._id,
+      },
+    ]
+    console.log('Seeding Project Data...')
+    const projects = await pMap(projectData, async (data) => {
+      const project = new Project(data)
+      await project.validate()
+      return project.save()
+    })
+
+    console.log('Updated Student Data (w/ Project Data)...')
+    const project = projects[0]
+    student.projects.push(project._id)
+    await student.validate()
+    await student.save()
 
     const userData = [
       {
         email: 'test@test.com',
-        password: 'testing'
-      }
+        password: 'testing',
+      },
     ]
-    const userPromises = userData.map(async (data) => {
-      try {
-        const user = new User(data)
-        return user.save()
-      } catch (e) {
-        throw e
-      }
+    console.log('Seeding User Data...')
+    await pMap(userData, async (data) => {
+      const user = new User(data)
+      await user.validate()
+      return user.save()
     })
-    await Promise.all(userPromises)
 
     console.log('Seeding completed.')
   } catch (e) {
