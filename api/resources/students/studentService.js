03a1ca5946c0e8ae6e86be0e5c7a0af5e7052935
Michael Perrotte <mike@npmjs.com>
2019-12-09 04:25:33 -0500

    feat(api,student,service): added service functions; linted

--- api/resources/students/studentService.js
+++ api/resources/students/studentService.js
@@ -1,12 +1,60 @@
-'use strict';
+'use strict'
 
-const { model: Student } = require('./studentModel');
+const { HTTP400Error } = require('../../utils/httpErrors')
 
-exports.listStudents = async () => {
+const { model: Student } = require('./studentModel')
+const { model: Cohort } = require('../cohorts/cohortModel')
+
+exports.getStudentById = async (id) => {
   try {
-    return Student.find({});
+    return Student.findById(id)
   } catch (e) {
-    throw e;
+    throw e
   }
-};
+}
 
+exports.getStudentProjects = async (id) => {
+  try {
+    const student = await Student
+      .findById(id)
+      .populate('projects')
+    return (student)
+      ? student.projects
+      : []
+  } catch (e) {
+    throw e
+  }
+}
+
+exports.listStudents = async ({ filter, include } = { filter: {} }) => {
+  try {
+    if (Array.isArray(include) && include.length) {
+      return include.reduce((acc, field) => {
+        return acc.populate(field)
+      }, Student.find(filter))
+    } else {
+      return Student.find(filter)
+    }
+  } catch (e) {
+    throw e
+  }
+}
+
+exports.createStudent = async (studentData) => {
+  try {
+    const { cohortId } = studentData
+    if (cohortId) {
+      const cohort = await Cohort.findById(cohortId)
+      if (cohort) {
+        const student = new Student(studentData)
+        await student.validate()
+        cohort.students.push(student._id)
+        await cohort.save()
+        return student.save()
+      }
+    }
+    throw new HTTP400Error()
+  } catch (e) {
+    throw e
+  }
+}
